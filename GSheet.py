a097c8d9e9a6017cfbaf14f7eb8e1d7929fd80e7
Rainith <rainey.lyons@gmail.com>
2020-11-23 17:59:23 -0600

    commenting out something on test

--- GSheet.py
+++ GSheet.py
@@ -5,7 +5,7 @@ gc = gspread.service_account(filename='credentials.json')
 Book = gc.open('Chess_Tourney')
 sheet = Book.get_worksheet(0)
 new_row = 6
-sheet.update('A6:B6', [0,0])
+#sheet.update('A6:B6', [0,0])
 print(len(sheet.col_values(1)))
 try:
     Cell = sheet.find("Rainey")

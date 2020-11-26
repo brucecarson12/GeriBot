3056ae6366f60df930e8ac843735cee0d8e1ede3
brucecarson12 <bcason1204@gmail.com>
2020-11-26 08:11:32 -0800

    commented out GSheet printing

--- GSheet.py
+++ GSheet.py
@@ -6,9 +6,9 @@ Book = gc.open('Chess_Tourney')
 sheet = Book.get_worksheet(0)
 new_row = 6
 #sheet.update('A6:B6', [0,0])
-print(len(sheet.col_values(1)))
-try:
-    Cell = sheet.find("Rainey")
-    print(f"{Cell.row},{Cell.col}")
-except:
-    print("nooooo")
+#print(len(sheet.col_values(1)))
+#try:
+#    Cell = sheet.find("Rainey")
+#    print(f"{Cell.row},{Cell.col}")
+#except:
+#    print("nooooo")

cf4440c20420567fe111c4aa998ec216d65c17de
Bryce Cason <bcason1204@gmail.com>
2021-01-24 18:05:57 -0800

    removed +

--- main.py
+++ main.py
@@ -259,7 +259,7 @@ async def on_message(message):
         analysis = str()
         if lastone['analysis'] != None:
             analysis = (f"""Average Centipawn Loss: {lastone['analysis']['acpl']} \nInaccuracies: ({lastone['analysis']['inaccuracy']}) {', '.join(lastone['badmoves']['inaccuracy'])} \nMistakes: ({lastone['analysis']['mistake']}) {', '.join(lastone['badmoves']['mistake'])} \nBlunders: ({lastone['analysis']['blunder']}) {', '.join(lastone['badmoves']['blunder'])}""")
-        await message.channel.send(f"<{lastone['link']}> \n{lastone['opening']}\n + {analysis}")
+        await message.channel.send(f"<{lastone['link']}> \n{lastone['opening']}\n{analysis}")
         await message.channel.send(lastone['gif'])
 
 

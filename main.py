c7d8cd456b492c03aceb70bc3284d47539c41687
Bryce Cason <bcason1204@gmail.com>
2021-11-17 22:18:33 -0800

    skipno changes

--- main.py
+++ main.py
@@ -171,13 +171,15 @@ async def findli(ctx,user1,user2):
 
 
 @bot.command()
-async def lastli(ctx, skipno=0):
+async def lastli(ctx, skipno=None):
     """This command grabs your last lichess game(based on start date). Takes # of Games to Skip input as well. (Ex. '$lastli 1' will skip your most recent game.)"""
     member = str(ctx.author)
     memberid = ctx.author.id
     Sheetinfo = UpdateSheetDiscordID(member,memberid)
     if isinstance(skipno, str):
+        print(skipno)
         skipno = 0
+        print(skipno)
     lastone = lastgame(Sheetinfo['lichess'],skipno)
     result = str()
     if lastone['status'] == 'draw':
